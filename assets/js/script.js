6ab7544d46428cb664f075b9f6a00895c2ea935b
Andre Igor <andreigormenezes@gmail.com>
2022-05-12 10:49:54 -0300

    animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,15 @@ const body = document.querySelector('body')
 const logoText = document.querySelector('.logo p')
 const themeButton = document.querySelector('#themeButton')
 
+const habContainer = document.querySelector('#habContainer')
+
 function changeTheme() {
     body.classList.toggle('dark')
     header.classList.toggle('dark')
     logoText.classList.toggle('dark')
     themeButton.classList.toggle('dark')
 }
+
+document.addEventListener('scroll', () => {
+    habContainer.classList.add('slide-animation')
+})
